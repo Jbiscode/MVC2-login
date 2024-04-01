4d9075063646aeb159bef99a542121a16633bfb6
Jbiscode <99472056+Jbiscode@users.noreply.github.com>
2024-04-01 17:52:54 +0900

    세션정보와 타임아웃 설정

--- src/main/java/hello/login/web/login/LoginController.java
+++ src/main/java/hello/login/web/login/LoginController.java
@@ -78,7 +78,7 @@ public class LoginController {
             bindingResult.reject("loginFail", "아이디 또는 비밀번호가 맞지 않습니다.");
             return "login/loginForm";
         }
-//로그인 성공 처리 TODO
+        //로그인 성공 처리 TODO
 
         // 세션이 있으면 있는 세션 반환, 없으면 신규 세션을 생성
         HttpSession session = request.getSession();
