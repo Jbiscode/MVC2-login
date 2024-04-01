461b5f3daf8aa8e6b674040a659b4a4adad9e20c
Jbiscode <99472056+Jbiscode@users.noreply.github.com>
2024-04-01 14:46:40 +0900

    회원가입

--- src/main/java/hello/login/TestDataInit.java
+++ src/main/java/hello/login/TestDataInit.java
@@ -2,16 +2,18 @@ package hello.login;
 
 import hello.login.domain.item.Item;
 import hello.login.domain.item.ItemRepository;
+import hello.login.domain.member.Member;
+import hello.login.domain.member.MemberRepository;
+import jakarta.annotation.PostConstruct;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
-
-import jakarta.annotation.PostConstruct;
 
 @Component
 @RequiredArgsConstructor
 public class TestDataInit {
 
     private final ItemRepository itemRepository;
+    private final MemberRepository memberRepository;
 
     /**
      * 테스트용 데이터 추가
@@ -20,6 +22,12 @@ public class TestDataInit {
     public void init() {
         itemRepository.save(new Item("itemA", 10000, 10));
         itemRepository.save(new Item("itemB", 20000, 20));
+
+        Member member = new Member();
+        member.setLoginId("test");
+        member.setName("테스터");
+        member.setPassword("test!");
+        memberRepository.save(member);
     }
 
 }
